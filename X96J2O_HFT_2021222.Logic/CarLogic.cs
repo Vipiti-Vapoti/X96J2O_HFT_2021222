a209a45d281e6c30c971cb08bc52b8e2e80384ba
Vipiti-Vapoti <matezsolt99@stud.uni-obuda.hu>
2022-05-02 19:33:54 +0200

    CarLogic done

---
+++ X96J2O_HFT_2021222.Logic/CarLogic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using X96J2O_HFT_2021222.Repository.Interfaces;
+using X96J2O_HFT_2021222.Repository;
+using X96J2O_HFT_2021222.Models;
+
+namespace X96J2O_HFT_2021222.Logic
+{
+    public class CarLogic 
+    {
+        IReporitory<Car> repo;
+
+        public CarLogic(IReporitory<Car> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Car item)
+        {
+   
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Car Read(int id)
+        {
+            var car = this.repo.Read(id);
+            if (car == null)
+            {
+                throw new ArgumentException("car not exist!");
+            }
+            return car;
+        }
+
+        public IQueryable<Car> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Car item)
+        {
+            this.repo.Update(item);
+        }
+        //NON CRUD-S
+        public double AVGRentPrice()
+        {
+            return this.repo.ReadAll().Average(c => c.RentPrice);
+        }
+
+        public IEnumerable<KeyValuePair<string, double>> AVGRentPriceByBrands()
+        {
+            return from x in repo.ReadAll()
+                   group x by x.Brand.Name into g
+                   select new KeyValuePair<string, double>
+                   (g.Key, g.Average(t => t.RentPrice));
+        }
+    }
+}
