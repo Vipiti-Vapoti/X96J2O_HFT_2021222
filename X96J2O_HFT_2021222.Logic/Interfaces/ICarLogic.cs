73f0ef054105169fcb467869ec9ff52289aa444c
Vipiti-Vapoti <matezsolt99@stud.uni-obuda.hu>
2022-05-02 19:48:26 +0200

    Interface added

---
+++ X96J2O_HFT_2021222.Logic/Interfaces/ICarLogic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using X96J2O_HFT_2021222.Models;
+
+namespace X96J2O_HFT_2021222.Logic
+{
+    public interface ICarLogic
+    {
+        double AVGRentPrice();
+        IEnumerable<KeyValuePair<string, double>> AVGRentPriceByBrands();
+        void Create(Car item);
+        void Delete(int id);
+        Car Read(int id);
+        IQueryable<Car> ReadAll();
+        void Update(Car item);
+    }
+}
