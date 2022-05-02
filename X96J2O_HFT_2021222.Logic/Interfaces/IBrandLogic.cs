3d51c3b646198936199bbca946dffb0540e50708
Vipiti-Vapoti <matezsolt99@stud.uni-obuda.hu>
2022-05-02 19:48:12 +0200

    Interface added

---
+++ X96J2O_HFT_2021222.Logic/Interfaces/IBrandLogic.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using X96J2O_HFT_2021222.Models;
+
+namespace X96J2O_HFT_2021222.Logic
+{
+    public interface IBrandLogic
+    {
+        void Create(Brand item);
+        void Delete(int id);
+        Brand Read(int id);
+        IQueryable<Brand> ReadAll();
+        void Update(Brand item);
+    }
+}
