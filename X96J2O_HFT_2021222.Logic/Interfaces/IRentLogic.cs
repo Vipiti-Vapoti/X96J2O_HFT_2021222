a170b8e1e6e8de7e702fe7d64f50c6f7bab9fd40
Vipiti-Vapoti <matezsolt99@stud.uni-obuda.hu>
2022-05-02 19:48:38 +0200

    smallfixes

---
+++ X96J2O_HFT_2021222.Logic/Interfaces/IRentLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using X96J2O_HFT_2021222.Models;
+
+namespace X96J2O_HFT_2021222.Logic
+{
+    public interface IRentLogic
+    {
+        void Create(Rent item);
+        void Delete(int id);
+        double? GetAvarageInComePerYear(int year);
+        List<int> HasToPayFine();
+        Rent Read(int id);
+        IQueryable<Rent> ReadAll();
+        List<int> StillOpenRentsById();
+        void Update(Rent item);
+    }
+}
