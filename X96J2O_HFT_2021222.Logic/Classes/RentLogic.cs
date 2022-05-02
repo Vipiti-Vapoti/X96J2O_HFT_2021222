d46e18e671220c233e012f86500dee5b6785c312
Vipiti-Vapoti <matezsolt99@stud.uni-obuda.hu>
2022-05-02 19:51:02 +0200

    small fix

--- X96J2O_HFT_2021222.Logic/Classes/RentLogic.cs
+++ X96J2O_HFT_2021222.Logic/Classes/RentLogic.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 namespace X96J2O_HFT_2021222.Logic
 {
-    public class RentLogic : IRentLogic
+    public class RentLogic 
     {
         IReporitory<Rent> repo;
 
