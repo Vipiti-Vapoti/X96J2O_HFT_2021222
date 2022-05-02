c8a7021221bd2568384206651b57c4a9aa42cbd9
Vipiti-Vapoti <matezsolt99@stud.uni-obuda.hu>
2022-05-02 19:47:21 +0200

    Small fixes

---
+++ X96J2O_HFT_2021222.Logic/Classes/BrandLogic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using X96J2O_HFT_2021222.Repository.Interfaces;
+using X96J2O_HFT_2021222.Repository;
+using X96J2O_HFT_2021222.Models;
+
+namespace X96J2O_HFT_2021222.Logic
+{
+    public class BrandLogic : IBrandLogic
+    {
+        IReporitory<Brand> repo;
+
+        public BrandLogic(IReporitory<Brand> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Brand item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Brand Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Brand> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Brand item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
